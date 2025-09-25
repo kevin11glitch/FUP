6d7bc376e4e2d6dea3872abf6b259e8e5441e39a
Kevin Mendes <118233405+kevin11glitch@users.noreply.github.com>
2025-09-25 13:32:28 -0300

    Delete 10.py

--- 10.py
+++
@@ -1,7 +0,0 @@
-# Faça uma função que receba um inteiro x e um inteiro não-negativo n e, usando laço de repetição, calcule x^n e retorne o resultado.
-
-def funcao(x,n):
-    num = x**n
-
-    return num
-
